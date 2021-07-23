7896926d405412d51ac550bf9dfa8e4eafc258b0
Dat H. Pham <dphamhoang@linagora.com>
2021-07-23 21:04:25 +0700

    Revert "Setup project for Jmap client in Dart (#2)" (#4)
    This reverts commit 9df2865d7bcb7f4d8dc9c1102a58064b9829fa33.

--- lib/jmap/core/request/ready_to_build.dart
+++
@@ -1,5 +0,0 @@
-import 'package:jmap_dart_client/jmap/core/request/request_object.dart';
-
-mixin ReadyToBuild {
-  RequestObject build();
-}
--- lib/util/util.dart
+++
@@ -1,4 +0,0 @@
-Iterable<int> get positiveIntegers sync* {
-  int i = 0;
-  while (true) yield i++;
-}
