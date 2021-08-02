f72210632bc8d6204e7a337a9b14acb8a937dcaf
dab246 <tdvu@linagora.com>
2021-08-02 17:01:55 +0700

    Directly call to /.well-known/jmap

--- lib/jmap/core/session/get_session.dart
+++ lib/jmap/core/session/get_session.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 import 'package:jmap_dart_client/http/converter/capabilities_converter.dart';
 import 'package:jmap_dart_client/http/http_client.dart';
 import 'package:jmap_dart_client/jmap/core/capability/capability_identifier.dart';
@@ -7,137 +5,34 @@ import 'package:jmap_dart_client/jmap/core/capability/capability_properties.dart
 import 'package:jmap_dart_client/jmap/core/session/session.dart';
 
 class GetSession {
-  final String jsonSession = '''
-  {
-    "capabilities": {
-      "urn:ietf:params:jmap:mail": {
-        "maxMailboxesPerEmail": 10000000,
-        "maxMailboxDepth": null,
-        "maxSizeMailboxName": 200,
-        "maxSizeAttachmentsPerEmail": 20000000,
-        "emailQuerySortOptions": [
-          "receivedAt",
-          "sentAt",
-          "size",
-          "from",
-          "to",
-          "subject"
-        ],
-        "mayCreateTopLevelMailbox": true
-      },
-      "urn:ietf:params:jmap:submission": {
-        "maxDelayedSend": 0,
-        "submissionExtensions": []
-      },
-      "urn:ietf:params:jmap:core": {
-        "maxSizeUpload": 31457280,
-        "maxConcurrentUpload": 4,
-        "maxSizeRequest": 10000000,
-        "maxConcurrentRequests": 4,
-        "maxCallsInRequest": 16,
-        "maxObjectsInGet": 500,
-        "maxObjectsInSet": 500,
-        "collationAlgorithms": [
-          "i;unicode-casemap"
-        ]
-      }
-    },
-    "accounts": {
-      "2bdee8dcc7f7a6817e2699d21c0462f3ab0b66d25de7e71ae2762d4ae8c591ba": {
-        "name": "benoit@localhost",
-        "isPersonal": true,
-        "isReadOnly": false,
-        "accountCapabilities": {
-          "urn:ietf:params:jmap:mail": {
-            "maxMailboxesPerEmail": 10000000,
-            "maxMailboxDepth": null,
-            "maxSizeMailboxName": 200,
-            "maxSizeAttachmentsPerEmail": 20000000,
-            "emailQuerySortOptions": [
-              "receivedAt",
-              "sentAt",
-              "size",
-              "from",
-              "to",
-              "subject"
-            ],
-            "mayCreateTopLevelMailbox": true
-          },
-          "urn:ietf:params:jmap:websocket": {
-            "supportsPush": true,
-            "url": "http://localhost/jmap/ws"
-          },
-          "urn:ietf:params:jmap:submission": {
-            "maxDelayedSend": 0,
-            "submissionExtensions": []
-          },
-          "urn:ietf:params:jmap:core": {
-            "maxSizeUpload": 31457280,
-            "maxConcurrentUpload": 4,
-            "maxSizeRequest": 10000000,
-            "maxConcurrentRequests": 4,
-            "maxCallsInRequest": 16,
-            "maxObjectsInGet": 500,
-            "maxObjectsInSet": 500,
-              "collationAlgorithms": ["i;unicode-casemap"]
-            },
-            "urn:ietf:params:jmap:vacationresponse": {}
-        }
-      }
-    },
-    "primaryAccounts": {
-      "urn:ietf:params:jmap:submission":
-          "2bdee8dcc7f7a6817e2699d21c0462f3ab0b66d25de7e71ae2762d4ae8c591ba",
-      "com:linagora:params:jmap:pgp":
-          "2bdee8dcc7f7a6817e2699d21c0462f3ab0b66d25de7e71ae2762d4ae8c591ba",
-      "urn:ietf:params:jmap:websocket":
-          "2bdee8dcc7f7a6817e2699d21c0462f3ab0b66d25de7e71ae2762d4ae8c591ba",
-      "com:linagora:params:jmap:ws:ticket":
-          "2bdee8dcc7f7a6817e2699d21c0462f3ab0b66d25de7e71ae2762d4ae8c591ba",
-      "urn:apache:james:params:jmap:mail:shares":
-          "2bdee8dcc7f7a6817e2699d21c0462f3ab0b66d25de7e71ae2762d4ae8c591ba",
-      "urn:ietf:params:jmap:vacationresponse":
-          "2bdee8dcc7f7a6817e2699d21c0462f3ab0b66d25de7e71ae2762d4ae8c591ba",
-      "urn:ietf:params:jmap:mail":
-          "2bdee8dcc7f7a6817e2699d21c0462f3ab0b66d25de7e71ae2762d4ae8c591ba",
-      "com:linagora:params:jmap:filter":
-          "2bdee8dcc7f7a6817e2699d21c0462f3ab0b66d25de7e71ae2762d4ae8c591ba",
-      "urn:ietf:params:jmap:mdn":
-          "2bdee8dcc7f7a6817e2699d21c0462f3ab0b66d25de7e71ae2762d4ae8c591ba",
-      "urn:apache:james:params:jmap:mail:quota":
-          "2bdee8dcc7f7a6817e2699d21c0462f3ab0b66d25de7e71ae2762d4ae8c591ba",
-      "urn:ietf:params:jmap:core":
-          "2bdee8dcc7f7a6817e2699d21c0462f3ab0b66d25de7e71ae2762d4ae8c591ba",
-      "com:linagora:params:jmap:echo":
-          "2bdee8dcc7f7a6817e2699d21c0462f3ab0b66d25de7e71ae2762d4ae8c591ba"
-    },
-    "username": "alice@localhost",
-    "apiUrl": "http://localhost/jmap",
-    "downloadUrl": "http://localhost/download/{accountId}/{blobId}/?type={type}&name={name}",
-    "uploadUrl": "http://localhost/upload/{accountId}",
-    "eventSourceUrl": "http://localhost/eventSource?types={types}&closeAfter={closeafter}&ping={ping}",
-    "state": "2c9f1b12-b35a-43e6-9af2-0106fb53a943"
-  }
-  ''';
-
   final HttpClient _httpClient;
+  final CapabilitiesConverter _capabilitiesConverter;
 
-  GetSession(this._httpClient);
-
-  final CapabilitiesConverter _capabilitiesConverter = CapabilitiesConverter();
-
-  void registerCapabilityConverter(Map<CapabilityIdentifier, CapabilityProperties Function(Map<String, dynamic>)> converters) {
-    _capabilitiesConverter.addConverters(converters);
-  }
+  GetSession(this._httpClient, this._capabilitiesConverter);
 
   Future<Session> execute() async {
-    return await getFakeSession()
-      .then((value) => Session.fromJson(value, converter: _capabilitiesConverter))
+    return await _httpClient.get('/.well-known/jmap')
+      .then((value) => extractData(value))
       .catchError((error) => throw error);
   }
 
-  Future<Map<String, dynamic>> getFakeSession() async {
-    final session = json.decode(jsonSession);
-    return Future.value(session);
+  Session extractData(Map<String, dynamic> body) {
+    return Session.fromJson(body, converter: _capabilitiesConverter);
+  }
+}
+
+class GetSessionBuilder {
+  final HttpClient _httpClient;
+  final CapabilitiesConverter _capabilitiesConverter = CapabilitiesConverter();
+
+  GetSessionBuilder(this._httpClient);
+
+  void registerCapabilityConverter(Map<CapabilityIdentifier, CapabilityProperties Function(Map<String, dynamic>)> converters) {
+    _capabilitiesConverter.addConverters(converters);
+    _capabilitiesConverter.build();
+  }
+
+  GetSession build() {
+    return GetSession(_httpClient, _capabilitiesConverter);
   }
 }
