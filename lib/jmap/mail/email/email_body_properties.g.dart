dd1f351f5085ace77b75d050d099bce3779c5e3d
dab246 <tdvu@linagora.com>
2021-08-06 11:56:08 +0700

    Implement GetEmailMethod

---
+++ lib/jmap/mail/email/email_body_properties.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'email_body_properties.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+EmailBodyProperties _$EmailBodyPropertiesFromJson(Map<String, dynamic> json) {
+  return EmailBodyProperties(
+    (json['value'] as List<dynamic>).map((e) => e as String).toSet(),
+  );
+}
+
+Map<String, dynamic> _$EmailBodyPropertiesToJson(
+        EmailBodyProperties instance) =>
+    <String, dynamic>{
+      'value': instance.value.toList(),
+    };
