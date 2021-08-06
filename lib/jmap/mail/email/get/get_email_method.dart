478a0b2d86f0e6159c89eb57abb2419aa745fb83
dab246 <tdvu@linagora.com>
2021-08-06 11:56:08 +0700

    Implement GetEmailMethod

---
+++ lib/jmap/mail/email/get/get_email_method.dart
@@ -0,0 +1,86 @@
+import 'package:jmap_dart_client/http/converter/account_id_converter.dart';
+import 'package:jmap_dart_client/http/converter/id_converter.dart';
+import 'package:jmap_dart_client/http/converter/properties_converter.dart';
+import 'package:jmap_dart_client/http/converter/unsigned_int_nullable_converter.dart';
+import 'package:jmap_dart_client/jmap/account_id.dart';
+import 'package:jmap_dart_client/jmap/core/capability/capability_identifier.dart';
+import 'package:jmap_dart_client/jmap/core/method/request/get_method.dart';
+import 'package:jmap_dart_client/jmap/core/request/request_invocation.dart';
+import 'package:jmap_dart_client/jmap/core/request/result_reference.dart';
+import 'package:jmap_dart_client/jmap/core/unsigned_int.dart';
+import 'package:jmap_dart_client/jmap/mail/email/email_body_properties.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'get_email_method.g.dart';
+
+@UnsignedIntNullableConverter()
+@IdConverter()
+@AccountIdConverter()
+@PropertiesConverter()
+@JsonSerializable(explicitToJson: true)
+class GetEmailMethod extends GetMethod with OptionalEmailBodyProperties, OptionalFetchTextBodyValues,
+    OptionalFetchHTMLBodyValues, OptionalFetchAllBodyValues, OptionalMaxBodyValueBytes {
+
+  GetEmailMethod(AccountId accountId) : super(accountId);
+
+  @override
+  MethodName get methodName => MethodName('Email/get');
+
+  @override
+  Set<CapabilityIdentifier> get requiredCapabilities => {
+    CapabilityIdentifier.jmapCore,
+    CapabilityIdentifier.jmapMail
+  };
+
+  @override
+  List<Object?> get props => [methodName, accountId, ids, properties, requiredCapabilities];
+
+  factory GetEmailMethod.fromJson(Map<String, dynamic> json) => _$GetEmailMethodFromJson(json);
+
+  Map<String, dynamic> toJson() => _$GetEmailMethodToJson(this);
+}
+
+mixin OptionalEmailBodyProperties {
+  @JsonKey(includeIfNull: false)
+  EmailBodyProperties? bodyProperties;
+
+  void addEmailBodyProperties(EmailBodyProperties value) {
+    bodyProperties = value;
+  }
+}
+
+mixin OptionalFetchTextBodyValues {
+  @JsonKey(includeIfNull: false)
+  bool? fetchTextBodyValues;
+
+  void addFetchTextBodyValues(bool value) {
+    fetchTextBodyValues = value;
+  }
+}
+
+mixin OptionalFetchHTMLBodyValues {
+  @JsonKey(includeIfNull: false)
+  bool? fetchHTMLBodyValues;
+
+  void addFetchHTMLBodyValues(bool value) {
+    fetchHTMLBodyValues = value;
+  }
+}
+
+mixin OptionalFetchAllBodyValues {
+  @JsonKey(includeIfNull: false)
+  bool? fetchAllBodyValues;
+
+  void addFetchAllBodyValues(bool value) {
+    fetchAllBodyValues = value;
+  }
+}
+
+mixin OptionalMaxBodyValueBytes {
+  @JsonKey(includeIfNull: false)
+  UnsignedInt? maxBodyValueBytes;
+
+  void addMaxBodyValueBytes(UnsignedInt value) {
+    maxBodyValueBytes = value;
+  }
+}
---
+++ lib/jmap/mail/email/get/get_email_method.g.dart
@@ -0,0 +1,58 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'get_email_method.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+GetEmailMethod _$GetEmailMethodFromJson(Map<String, dynamic> json) {
+  return GetEmailMethod(
+    const AccountIdConverter().fromJson(json['accountId'] as String),
+  )
+    ..ids = (json['ids'] as List<dynamic>?)
+        ?.map((e) => const IdConverter().fromJson(e as String))
+        .toSet()
+    ..referenceIds = json['#ids'] == null
+        ? null
+        : ResultReference.fromJson(json['#ids'] as Map<String, dynamic>)
+    ..properties = const PropertiesConverter()
+        .fromJson(json['properties'] as List<String>?)
+    ..referenceProperties = json['#properties'] == null
+        ? null
+        : ResultReference.fromJson(json['#properties'] as Map<String, dynamic>)
+    ..bodyProperties = json['bodyProperties'] == null
+        ? null
+        : EmailBodyProperties.fromJson(
+            json['bodyProperties'] as Map<String, dynamic>)
+    ..fetchTextBodyValues = json['fetchTextBodyValues'] as bool?
+    ..fetchHTMLBodyValues = json['fetchHTMLBodyValues'] as bool?
+    ..fetchAllBodyValues = json['fetchAllBodyValues'] as bool?
+    ..maxBodyValueBytes = const UnsignedIntNullableConverter()
+        .fromJson(json['maxBodyValueBytes'] as int?);
+}
+
+Map<String, dynamic> _$GetEmailMethodToJson(GetEmailMethod instance) {
+  final val = <String, dynamic>{
+    'accountId': const AccountIdConverter().toJson(instance.accountId),
+  };
+
+  void writeNotNull(String key, dynamic value) {
+    if (value != null) {
+      val[key] = value;
+    }
+  }
+
+  writeNotNull('ids', instance.ids?.map(const IdConverter().toJson).toList());
+  writeNotNull('#ids', instance.referenceIds?.toJson());
+  writeNotNull(
+      'properties', const PropertiesConverter().toJson(instance.properties));
+  writeNotNull('#properties', instance.referenceProperties?.toJson());
+  writeNotNull('bodyProperties', instance.bodyProperties?.toJson());
+  writeNotNull('fetchTextBodyValues', instance.fetchTextBodyValues);
+  writeNotNull('fetchHTMLBodyValues', instance.fetchHTMLBodyValues);
+  writeNotNull('fetchAllBodyValues', instance.fetchAllBodyValues);
+  writeNotNull('maxBodyValueBytes',
+      const UnsignedIntNullableConverter().toJson(instance.maxBodyValueBytes));
+  return val;
+}
