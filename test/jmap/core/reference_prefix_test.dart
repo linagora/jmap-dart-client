777c67aa01470e3dcd561c0156632fc0cca61477
dab246 <tdvu@linagora.com>
2021-08-31 15:33:14 +0700

    TF-50 Add unit test for set email submission

---
+++ test/jmap/core/reference_prefix_test.dart
@@ -0,0 +1,23 @@
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:jmap_dart_client/jmap/core/reference_prefix.dart';
+
+void main() {
+  group('Invalid reference_prefix', () {
+    test('Should throw argument error when the reference_prefix is empty', () {
+      expect(() => ReferencePrefix(''), throwsArgumentError);
+    });
+
+    test('Should throw argument error when the reference_prefix has length more than 255', () {
+      expect(
+        () => ReferencePrefix('a123bde34588799922229339933933933993939393939222a123bde34'
+          '588799922229339933933933993939393939222a123be3458879992222933993393393'
+          '3993939393939222a123bde34588799922229339933933933993939393939222a123bd'
+          'e34588799922229339933933933993939393939222a123bde345887999222293399339'
+          '33933993939393939222a123bde34588799922229339933933933993939393939222a1'
+          '23bde34588799922229339933933933993939393939222111111111111111111111111'),
+        throwsArgumentError
+      );
+    });
+  });
+}
