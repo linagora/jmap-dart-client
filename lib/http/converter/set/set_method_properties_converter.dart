09b37d3b3cc7c6f6bd698375ac37139579dc3656
dab246 <tdvu@linagora.com>
2021-08-31 11:45:51 +0700

    TF-50 Implement set email submission method and response

--- lib/http/converter/set/set_method_properties_converter.dart
+++ lib/http/converter/set/set_method_properties_converter.dart
@@ -1,8 +1,19 @@
 import 'package:jmap_dart_client/http/converter/id_converter.dart';
+import 'package:jmap_dart_client/http/converter/references_email_submission_id_converter.dart';
 import 'package:jmap_dart_client/jmap/core/id.dart';
+import 'package:jmap_dart_client/jmap/core/patch_object.dart';
+import 'package:jmap_dart_client/jmap/mail/email/submission/email_submission_id.dart';
 
 class SetMethodPropertiesConverter {
   MapEntry<String, dynamic> fromMapIdToJson(Id id, dynamic object) {
     return MapEntry(const IdConverter().toJson(id), object);
+  }
+
+  MapEntry<String, dynamic> fromMapEmailSubmissionIdToJson(EmailSubmissionId emailSubmissionId, dynamic object) {
+    if (object is PatchObject) {
+      return MapEntry(const ReferencesEmailSubmissionIdConverter().toJson(emailSubmissionId), object.toJson());
+    } else {
+      return MapEntry(const ReferencesEmailSubmissionIdConverter().toJson(emailSubmissionId), object);
+    }
   }
 }
---
+++ lib/jmap/mail/email/submission/set/set_email_submission_method.dart
@@ -0,0 +1,86 @@
+
+import 'package:jmap_dart_client/http/converter/account_id_converter.dart';
+import 'package:jmap_dart_client/http/converter/id_converter.dart';
+import 'package:jmap_dart_client/http/converter/references_email_submission_id_converter.dart';
+import 'package:jmap_dart_client/http/converter/set/set_method_properties_converter.dart';
+import 'package:jmap_dart_client/jmap/account_id.dart';
+import 'package:jmap_dart_client/jmap/core/capability/capability_identifier.dart';
+import 'package:jmap_dart_client/jmap/core/method/request/set_method.dart';
+import 'package:jmap_dart_client/jmap/core/patch_object.dart';
+import 'package:jmap_dart_client/jmap/core/request/request_invocation.dart';
+import 'package:jmap_dart_client/jmap/mail/email/submission/email_submission.dart';
+import 'package:jmap_dart_client/jmap/mail/email/submission/email_submission_id.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+class SetEmailSubmissionMethod extends SetMethod<EmailSubmission> with OptionalOnSuccessUpdateEmail, OptionalOnSuccessDestroyEmail  {
+  SetEmailSubmissionMethod(AccountId accountId) : super(accountId);
+
+  @override
+  MethodName get methodName => MethodName('EmailSubmission/set');
+
+  @override
+  Set<CapabilityIdentifier> get requiredCapabilities => {
+    CapabilityIdentifier.jmapSubmission,
+    CapabilityIdentifier.jmapMail,
+    CapabilityIdentifier.jmapCore
+  };
+
+  @override
+  Map<String, dynamic> toJson() {
+    final val = <String, dynamic>{
+      'accountId': const AccountIdConverter().toJson(accountId),
+    };
+
+    void writeNotNull(String key, dynamic value) {
+      if (value != null) {
+        val[key] = value;
+      }
+    }
+
+    writeNotNull('ifInState', ifInState?.value);
+    writeNotNull('create', create
+        ?.map((id, create) => SetMethodPropertiesConverter()
+        .fromMapIdToJson(id, create.toJson())));
+    writeNotNull('update', update
+        ?.map((id, update) => SetMethodPropertiesConverter()
+        .fromMapIdToJson(id, update.toJson())));
+    writeNotNull('destroy', destroy
+        ?.map((destroyId) => IdConverter()
+        .toJson(destroyId)).toList());
+    writeNotNull('onSuccessUpdateEmail', onSuccessUpdateEmail
+        ?.map((id, update) => SetMethodPropertiesConverter()
+        .fromMapEmailSubmissionIdToJson(id, update)));
+    writeNotNull('onSuccessDestroyEmail', onSuccessDestroyEmail
+        ?.map((destroyId) => ReferencesEmailSubmissionIdConverter()
+        .toJson(destroyId)).toList());
+
+    return val;
+  }
+
+  @override
+  List<Object?> get props => [accountId, ifInState, create, update, destroy, onSuccessUpdateEmail, onSuccessDestroyEmail];
+}
+
+mixin OptionalOnSuccessUpdateEmail {
+  @JsonKey(includeIfNull: false)
+  Map<EmailSubmissionId, PatchObject>? onSuccessUpdateEmail;
+
+  void addOnSuccessUpdateEmail(Map<EmailSubmissionId, PatchObject> values) {
+    if (onSuccessUpdateEmail == null) {
+      onSuccessUpdateEmail = Map<EmailSubmissionId, PatchObject>();
+    }
+    onSuccessUpdateEmail?.addAll(values);
+  }
+}
+
+mixin OptionalOnSuccessDestroyEmail {
+  @JsonKey(includeIfNull: false)
+  Set<EmailSubmissionId>? onSuccessDestroyEmail;
+
+  void addOnSuccessDestroyEmail(Set<EmailSubmissionId> values) {
+    if (onSuccessDestroyEmail == null) {
+      onSuccessDestroyEmail = Set();
+    }
+    onSuccessDestroyEmail?.addAll(values);
+  }
+}
