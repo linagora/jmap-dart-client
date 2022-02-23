363a6d8e28fe2632f4e2da38acd6da6bdf50f57a
dab246 <tdvu@linagora.com>
2022-02-23 09:07:31 +0700

    Create set mailbox method and response

--- lib/jmap/mail/mailbox/mailbox.g.dart
+++ lib/jmap/mail/mailbox/mailbox.g.dart
@@ -6,38 +6,54 @@ part of 'mailbox.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-Mailbox _$MailboxFromJson(Map<String, dynamic> json) {
-  return Mailbox(
-    const MailboxIdConverter().fromJson(json['id'] as String),
-    const MailboxNameConverter().fromJson(json['name'] as String?),
-    const MailboxIdNullableConverter().fromJson(json['parentId'] as String?),
-    const RoleConverter().fromJson(json['role'] as String?),
-    const SortOrderConverter().fromJson(json['sortOrder'] as int?),
-    const TotalEmailConverter().fromJson(json['totalEmails'] as int?),
-    const UnreadEmailsConverter().fromJson(json['unreadEmails'] as int?),
-    const TotalThreadsConverter().fromJson(json['totalThreads'] as int?),
-    const UnreadThreadsConverter().fromJson(json['unreadThreads'] as int?),
-    json['myRights'] == null
-        ? null
-        : MailboxRights.fromJson(json['myRights'] as Map<String, dynamic>),
-    const IsSubscribedConverter().fromJson(json['isSubscribed'] as bool?),
-  );
-}
+Mailbox _$MailboxFromJson(Map<String, dynamic> json) => Mailbox(
+      id: const MailboxIdNullableConverter().fromJson(json['id'] as String?),
+      name: const MailboxNameConverter().fromJson(json['name'] as String?),
+      parentId: const MailboxIdNullableConverter()
+          .fromJson(json['parentId'] as String?),
+      role: const RoleConverter().fromJson(json['role'] as String?),
+      sortOrder: const SortOrderConverter().fromJson(json['sortOrder'] as int?),
+      totalEmails:
+          const TotalEmailConverter().fromJson(json['totalEmails'] as int?),
+      unreadEmails:
+          const UnreadEmailsConverter().fromJson(json['unreadEmails'] as int?),
+      totalThreads:
+          const TotalThreadsConverter().fromJson(json['totalThreads'] as int?),
+      unreadThreads: const UnreadThreadsConverter()
+          .fromJson(json['unreadThreads'] as int?),
+      myRights: json['myRights'] == null
+          ? null
+          : MailboxRights.fromJson(json['myRights'] as Map<String, dynamic>),
+      isSubscribed:
+          const IsSubscribedConverter().fromJson(json['isSubscribed'] as bool?),
+    );
 
-Map<String, dynamic> _$MailboxToJson(Mailbox instance) => <String, dynamic>{
-      'id': const MailboxIdConverter().toJson(instance.id),
-      'name': const MailboxNameConverter().toJson(instance.name),
-      'parentId': const MailboxIdNullableConverter().toJson(instance.parentId),
-      'role': const RoleConverter().toJson(instance.role),
-      'sortOrder': const SortOrderConverter().toJson(instance.sortOrder),
-      'totalEmails': const TotalEmailConverter().toJson(instance.totalEmails),
-      'unreadEmails':
-          const UnreadEmailsConverter().toJson(instance.unreadEmails),
-      'totalThreads':
-          const TotalThreadsConverter().toJson(instance.totalThreads),
-      'unreadThreads':
-          const UnreadThreadsConverter().toJson(instance.unreadThreads),
-      'myRights': instance.myRights,
-      'isSubscribed':
-          const IsSubscribedConverter().toJson(instance.isSubscribed),
-    };
+Map<String, dynamic> _$MailboxToJson(Mailbox instance) {
+  final val = <String, dynamic>{};
+
+  void writeNotNull(String key, dynamic value) {
+    if (value != null) {
+      val[key] = value;
+    }
+  }
+
+  writeNotNull('id', const MailboxIdNullableConverter().toJson(instance.id));
+  writeNotNull('name', const MailboxNameConverter().toJson(instance.name));
+  writeNotNull(
+      'parentId', const MailboxIdNullableConverter().toJson(instance.parentId));
+  writeNotNull('role', const RoleConverter().toJson(instance.role));
+  writeNotNull(
+      'sortOrder', const SortOrderConverter().toJson(instance.sortOrder));
+  writeNotNull(
+      'totalEmails', const TotalEmailConverter().toJson(instance.totalEmails));
+  writeNotNull('unreadEmails',
+      const UnreadEmailsConverter().toJson(instance.unreadEmails));
+  writeNotNull('totalThreads',
+      const TotalThreadsConverter().toJson(instance.totalThreads));
+  writeNotNull('unreadThreads',
+      const UnreadThreadsConverter().toJson(instance.unreadThreads));
+  writeNotNull('myRights', instance.myRights);
+  writeNotNull('isSubscribed',
+      const IsSubscribedConverter().toJson(instance.isSubscribed));
+  return val;
+}
