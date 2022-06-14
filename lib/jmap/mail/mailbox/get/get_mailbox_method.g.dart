a8b8f871074a684c8f358b4de699fad35a0ee280
ManhNT <ManhNT@MNTs-MacBook-Pro.local>
2022-06-14 13:24:37 +0700

    TF-626: create unit test for test case get changes mailbox

--- lib/jmap/mail/mailbox/get/get_mailbox_method.g.dart
+++ lib/jmap/mail/mailbox/get/get_mailbox_method.g.dart
@@ -6,22 +6,22 @@ part of 'get_mailbox_method.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-GetMailboxMethod _$GetMailboxMethodFromJson(Map<String, dynamic> json) {
-  return GetMailboxMethod(
-    const AccountIdConverter().fromJson(json['accountId'] as String),
-  )
-    ..ids = (json['ids'] as List<dynamic>?)
-        ?.map((e) => const IdConverter().fromJson(e as String))
-        .toSet()
-    ..referenceIds = json['#ids'] == null
-        ? null
-        : ResultReference.fromJson(json['#ids'] as Map<String, dynamic>)
-    ..properties = const PropertiesConverter()
-        .fromJson(json['properties'] as List<String>?)
-    ..referenceProperties = json['#properties'] == null
-        ? null
-        : ResultReference.fromJson(json['#properties'] as Map<String, dynamic>);
-}
+GetMailboxMethod _$GetMailboxMethodFromJson(Map<String, dynamic> json) =>
+    GetMailboxMethod(
+      const AccountIdConverter().fromJson(json['accountId'] as String),
+    )
+      ..ids = (json['ids'] as List<dynamic>?)
+          ?.map((e) => const IdConverter().fromJson(e as String))
+          .toSet()
+      ..referenceIds = json['#ids'] == null
+          ? null
+          : ResultReference.fromJson(json['#ids'] as Map<String, dynamic>)
+      ..properties = const PropertiesConverter()
+          .fromJson(json['properties'] as List<String>?)
+      ..referenceProperties = json['#properties'] == null
+          ? null
+          : ResultReference.fromJson(
+              json['#properties'] as Map<String, dynamic>);
 
 Map<String, dynamic> _$GetMailboxMethodToJson(GetMailboxMethod instance) {
   final val = <String, dynamic>{
@@ -35,9 +35,9 @@ Map<String, dynamic> _$GetMailboxMethodToJson(GetMailboxMethod instance) {
   }
 
   writeNotNull('ids', instance.ids?.map(const IdConverter().toJson).toList());
-  writeNotNull('#ids', instance.referenceIds);
+  writeNotNull('#ids', instance.referenceIds?.toJson());
   writeNotNull(
       'properties', const PropertiesConverter().toJson(instance.properties));
-  writeNotNull('#properties', instance.referenceProperties);
+  writeNotNull('#properties', instance.referenceProperties?.toJson());
   return val;
 }
