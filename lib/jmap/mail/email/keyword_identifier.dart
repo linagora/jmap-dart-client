f4f4aee5f26c3cc7ad0c57df4c24b70286553488
dab246 <tdvu@linagora.com>
2022-10-07 12:53:45 +0700

    Implement `MDN/send` method/response

--- lib/jmap/mail/email/keyword_identifier.dart
+++ lib/jmap/mail/email/keyword_identifier.dart
@@ -1,4 +1,5 @@
 import 'package:equatable/equatable.dart';
+import 'package:jmap_dart_client/jmap/core/patch_object.dart';
 
 class KeyWordIdentifier with EquatableMixin {
   static final emailDraft = KeyWordIdentifier("\$draft");
@@ -9,10 +10,15 @@ class KeyWordIdentifier with EquatableMixin {
   static final emailPhishing = KeyWordIdentifier("\$phishing");
   static final emailJunk = KeyWordIdentifier("\$junk");
   static final emailNotJunk = KeyWordIdentifier("\$notjunk");
+  static final mdnSent = KeyWordIdentifier("\$mdnsent");
 
   final String value;
 
   KeyWordIdentifier(this.value);
+
+  String toPatchObjectJson() {
+    return '${PatchObject.keywordsProperty}/$value';
+  }
 
   @override
   List<Object> get props => [value];
