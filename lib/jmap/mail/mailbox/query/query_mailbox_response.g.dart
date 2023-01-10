92982e13c9414c71e14451da83668b05c282fc2b
HuyNguyen <quanghnguyen@linagora.com>
2023-01-10 12:53:33 +0700

    TF-1362: Query mailbox spam report

---
+++ lib/jmap/mail/mailbox/query/query_mailbox_response.g.dart
@@ -0,0 +1,33 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'query_mailbox_response.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+QueryMailboxResponse _$QueryMailboxResponseFromJson(
+        Map<String, dynamic> json) =>
+    QueryMailboxResponse(
+      const AccountIdConverter().fromJson(json['accountId'] as String),
+      const StateConverter().fromJson(json['queryState'] as String),
+      json['canCalculateChanges'] as bool,
+      const UnsignedIntConverter().fromJson(json['position'] as int),
+      (json['ids'] as List<dynamic>)
+          .map((e) => const IdConverter().fromJson(e as String))
+          .toSet(),
+      const UnsignedIntConverter().fromJson(json['total'] as int),
+      const UnsignedIntConverter().fromJson(json['limit'] as int),
+    );
+
+Map<String, dynamic> _$QueryMailboxResponseToJson(
+        QueryMailboxResponse instance) =>
+    <String, dynamic>{
+      'accountId': const AccountIdConverter().toJson(instance.accountId),
+      'queryState': const StateConverter().toJson(instance.queryState),
+      'canCalculateChanges': instance.canCalculateChanges,
+      'position': const UnsignedIntConverter().toJson(instance.position),
+      'ids': instance.ids.map(const IdConverter().toJson).toList(),
+      'total': const UnsignedIntConverter().toJson(instance.total),
+      'limit': const UnsignedIntConverter().toJson(instance.limit),
+    };
