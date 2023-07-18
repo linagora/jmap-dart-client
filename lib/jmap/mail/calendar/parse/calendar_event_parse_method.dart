22e4a0d393e61a0b42c7e05b6ea09eae88199471
dab246 <tdvu@linagora.com>
2023-07-18 17:09:50 +0700

    Implement parse method and response

---
+++ lib/jmap/mail/calendar/parse/calendar_event_parse_method.dart
@@ -0,0 +1,32 @@
+import 'package:jmap_dart_client/http/converter/account_id_converter.dart';
+import 'package:jmap_dart_client/http/converter/id_converter.dart';
+import 'package:jmap_dart_client/jmap/core/capability/capability_identifier.dart';
+import 'package:jmap_dart_client/jmap/core/method/request/parse_method.dart';
+import 'package:jmap_dart_client/jmap/core/request/request_invocation.dart';
+import 'package:jmap_dart_client/jmap/mail/calendar/calendar_event.dart';
+
+class CalendarEventParseMethod extends ParseMethod<CalendarEvent> {
+
+  CalendarEventParseMethod(super.accountId, super.blobIds);
+
+  @override
+  MethodName get methodName => MethodName('CalendarEvent/parse');
+
+  @override
+  Set<CapabilityIdentifier> get requiredCapabilities => {
+    CapabilityIdentifier.jmapCore,
+    CapabilityIdentifier.jamesCalendarEvent
+  };
+
+  @override
+  Map<String, dynamic> toJson() {
+    final val = <String, dynamic>{
+      'accountId': const AccountIdConverter().toJson(accountId),
+      'blobIds': blobIds.map(const IdConverter().toJson).toList()
+    };
+    return val;
+  }
+
+  @override
+  List<Object?> get props => [accountId, blobIds];
+}
