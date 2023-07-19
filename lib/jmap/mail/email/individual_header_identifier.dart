38feef3ca03d7ca8945ae2420f13e074f778d112
dab246 <tdvu@linagora.com>
2023-07-19 15:08:13 +0700

    Add headerCalendarEvent property for email

--- lib/jmap/mail/email/individual_header_identifier.dart
+++ lib/jmap/mail/email/individual_header_identifier.dart
@@ -3,6 +3,7 @@ import 'package:equatable/equatable.dart';
 class IndividualHeaderIdentifier with EquatableMixin {
   static final headerUserAgent = IndividualHeaderIdentifier('header:User-Agent:asText');
   static final headerMdn = IndividualHeaderIdentifier('header:Disposition-Notification-To:asText');
+  static final headerCalendarEvent = IndividualHeaderIdentifier('header:X-MEETING-UID:asText');
 
   final String value;
 
