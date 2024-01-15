f852f0bf33961ba1f609575bf81665c50ca7fa59
dab246 <tdvu@linagora.com>
2024-01-15 14:00:48 +0700

    Add `Accept-Language` and `Content-Language` header to email body values

--- lib/jmap/mail/email/individual_header_identifier.dart
+++ lib/jmap/mail/email/individual_header_identifier.dart
@@ -4,6 +4,8 @@ class IndividualHeaderIdentifier with EquatableMixin {
   static final headerUserAgent = IndividualHeaderIdentifier('header:User-Agent:asText');
   static final headerMdn = IndividualHeaderIdentifier('header:Disposition-Notification-To:asText');
   static final headerCalendarEvent = IndividualHeaderIdentifier('header:X-MEETING-UID:asText');
+  static final acceptLanguageHeader = IndividualHeaderIdentifier('header:Accept-Language:asText');
+  static final contentLanguageHeader = IndividualHeaderIdentifier('header:Content-Language:asText');
 
   final String value;
 
