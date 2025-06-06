586e10a899a496d1c66442ca983c7ec6e9d42716
DatDang <tddang@linagora.com>
2025-06-06 10:38:06 +0700

    Add Return-Path header

--- lib/jmap/mail/email/individual_header_identifier.dart
+++ lib/jmap/mail/email/individual_header_identifier.dart
@@ -3,6 +3,7 @@ import 'package:equatable/equatable.dart';
 class IndividualHeaderIdentifier with EquatableMixin {
   static final headerUserAgent = IndividualHeaderIdentifier('header:User-Agent:asText');
   static final headerMdn = IndividualHeaderIdentifier('header:Disposition-Notification-To:asText');
+  static final headerReturnPath = IndividualHeaderIdentifier('header:Return-Path:asText');
   static final headerCalendarEvent = IndividualHeaderIdentifier('header:X-MEETING-UID:asText');
   static final acceptLanguageHeader = IndividualHeaderIdentifier('header:Accept-Language:asText');
   static final contentLanguageHeader = IndividualHeaderIdentifier('header:Content-Language:asText');
